dc8fdf32e3856374ea9ac02c1be77dc4ff094d45
rosso101 <victorprivv101@gmail.com>
2023-10-31 08:42:32 +0300

    completed assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,34 @@
-import React from "react";
+// import React from "react";
+
+// function QuestionList() {
+//   return (
+//     <section>
+//       <h1>Quiz Questions</h1>
+//       <ul>{/* display QuestionItem components here after fetching */}</ul>
+//     </section>
+//   );
+// }
+
+// export default QuestionList;
+
+import React, { useState, useEffect } from 'react';
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  // Render the list of questions here
   return (
-    <section>
-      <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
-    </section>
+    <div>
+      {questions.map((question) => (
+        <div key={question.id}>{question.prompt}</div>
+      ))}
+    </div>
   );
 }
 
